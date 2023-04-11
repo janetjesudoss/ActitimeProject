62fbe51cff73a7edf6ac75c75c1056477d08c62d
janetjesudoss <117417289+janetjesudoss@users.noreply.github.com>
2023-04-11 15:32:39 +0530

    Create CreateTask.java

---
+++ Automation/src/com/actitime/testscript/CreateTask.java
@@ -0,0 +1,16 @@
+package com.actitime.testscript;
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CreateTask {
+	@Test
+	public void createTask()
+	{
+		Reporter.log("createTask",true);
+		Assert.fail();
+	}
+
+
+}
