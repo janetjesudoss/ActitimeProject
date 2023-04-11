eb0a6ca1a330a2bb4068cc55e3a41e81be9e16f6
janet <janet@janet>
2023-04-11 15:34:42 +0530

    CreateCustomerTC112

---
+++ Automation/src/com/actitime/testscript/CreateCustomer.java
@@ -0,0 +1,16 @@
+package com.actitime.testscript;
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CreateCustomer {
+	@Test
+	public void createCustomer()
+	{
+		Reporter.log("createCustomer",true);
+		Assert.fail();
+	}
+
+
+}
